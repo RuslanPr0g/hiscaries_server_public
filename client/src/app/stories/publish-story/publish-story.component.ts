6c28982683f98ddba79368c91ce17921c35f3998
Ruslan Rudenko <theruslan.prog@gmail.com>
2024-08-01 19:15:41 +0200

    add structure

---
+++ client/src/app/stories/publish-story/publish-story.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-publish-story',
+  standalone: true,
+  imports: [],
+  templateUrl: './publish-story.component.html',
+  styleUrl: './publish-story.component.scss'
+})
+export class PublishStoryComponent {
+
+}
